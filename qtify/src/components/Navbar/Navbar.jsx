045d5412c8bdf092937e5e89421fa12691fce2ac
Subhankar Dutta <duttasubhankar999@gmail.com>
2024-01-23 00:26:42 +0530

    commented the a tag in navbar component

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -7,11 +7,11 @@ import Feedback from "../Feedback/Feedback";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <a href="/" className={styles.navbar_links}>
-        <Logo />
-        <Searchbar placeholder={"Search a album of your choice"} />
-        <Feedback text={"Give Feedback"} />
-      </a>
+      {/* <a href="/" className={styles.navbar_links}> */}
+      <Logo />
+      <Searchbar placeholder={"Search a album of your choice"} />
+      <Feedback text={"Give Feedback"} />
+      {/* </a> */}
     </nav>
   );
 };
